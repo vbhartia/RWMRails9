5dc7063c3d79fb25e195468769824f71da3c90cc
Varun Bhartia <vbhartia@mba2014.hbs.edu>
2013-07-26 20:19:32 -0400

    fixed routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  def after_sign_in_path_for(resource)
-  	'/articles/'
+  after_filter :store_location
 
+  def store_location
+ 	# store last url - this is needed for post-login redirect to whatever the user last visited.
+    if (request.fullpath != "/login" && 
+       (request.fullpath != "/logout" &&
+        !request.xhr?)) # don't store ajax calls
+      session[:previous_url] = request.fullpath 
+    end
+  end
+
+
+  def after_sign_in_path_for(resource)
+  	session[:previous_url] || '/articles/'
   end
 
 private 

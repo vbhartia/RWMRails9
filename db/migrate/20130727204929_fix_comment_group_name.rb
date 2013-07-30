4dc3c1e31fd19954f1da5d147c0cc8e1a9e33dd5
Varun Bhartia <vbhartia@mba2014.hbs.edu>
2013-07-30 08:13:48 -0400

    fix migration

--- db/migrate/20130727204929_fix_comment_group_name.rb
+++ db/migrate/20130727204929_fix_comment_group_name.rb
@@ -1,6 +1,6 @@
 class FixCommentGroupName < ActiveRecord::Migration
   def change
-  	remove_column :comments, :comment_group, :integer
+  	remove_column :comments, :comment_group
   	add_column :comments, :group_comment_id, :integer
   end
 

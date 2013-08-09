c390163e2c1345fcb4150b82cd0c5f13e33d8ffe
Varun Bhartia <vbhartia@mba2014.hbs.edu>
2013-08-09 04:04:35 -0400

    add slug, fix facebook

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,6 +5,21 @@ class Article < ActiveRecord::Base
 
   attr_accessible :headline, :content, :imageURL, :originalURL, :user_id
 
+  after_save :generate_slug
+
+  def to_param
+    slug
+  end
+
+  def generate_slug
+    if (!self.slug) 
+		slug_string = id.to_s + '-' + self.headline.parameterize
+    	self.slug ||= slug_string
+	    self.save
+	    puts 'after save'
+ 	end
+
+  end
 
   def to_param
     "#{id}-#{headline}".parameterize
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130808201405) do
+ActiveRecord::Schema.define(:version => 20130808211345) do
 
   create_table "activities", :force => true do |t|
     t.integer  "user_id"
@@ -36,8 +36,10 @@ ActiveRecord::Schema.define(:version => 20130808201405) do
     t.text     "description"
     t.string   "author"
     t.string   "site_name"
+    t.string   "slug"
   end
 
+  add_index "articles", ["slug"], :name => "index_articles_on_slug"
   add_index "articles", ["user_id"], :name => "index_articles_on_user_id"
 
   create_table "comments", :force => true do |t|

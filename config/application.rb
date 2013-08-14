7d95fc53b07147df80af33029e7fe6f4eea2ffe3
Varun Bhartia <vbhartia@mba2014.hbs.edu>
2013-08-14 14:31:06 -0400

    fix homepage

--- config/application.rb
+++ config/application.rb
@@ -59,7 +59,11 @@ module Rwm9
 
     # Enable the asset pipeline
     config.assets.enabled = true
-    config.assets.paths << "#{Rails.root}/app/assets/fonts"
+        # Add the fonts path
+    config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+
+    # Precompile additional assets
+    config.assets.precompile += %w( .svg .eot .woff .ttf )
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
